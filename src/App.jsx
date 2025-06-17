eb716b621f6bd63869f07c2f68ae8ce74995a04b
SourovKarmokar <sourovkarmokar22@gmail.com>
2025-06-18 02:08:43 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import registration from "./assets/registration.png"
+
+function App() {
+  return (
+    <div className="flex">
+      <div className="w-[60%] pt-[225px] pl-[90px]">
+        <h2 className="font-secondary font-bold text-secondary text-[34px]">Get started with easily register</h2>
+        <p className="font-secondary text-[20px] text-black/50 mt-[13px]">Free register and you can enjoy it</p>
+
+        <div>
+
+          <div class="relative my-[34px]">
+            <input type="text" id="floating_outlined2" class="block px-[26px] py-[26px] xl:w-[368px] text-xl text-secondary font-semibold bg-transparent rounded-lg border-1 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-secondary/30 peer" placeholder=" " />
+
+            <label for="floating_outlined2" class="absolute text-sm text-secondary/70 duration-300 transform  -translate-y-4 top-2 z-10 origin-[0] bg-white  px-4 peer-focus:px-4 peer-focus:text-secondary/70  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2  peer-focus:-translate-y-4 rtl:peer-focus:translate-x-2/5 rtl:peer-focus:left-auto start-4">Full Name</label>
+          </div>
+
+        </div>
+
+      </div>
+      <div className="w-[40%]">
+        <img className="w-full h-screen object-cover" src={registration} alt="#registration" />
+      </div>
+    </div>
+  )
+}
+
+export default App
