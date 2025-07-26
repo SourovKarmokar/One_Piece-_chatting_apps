362e0ccc56974823eea96f8bece2188c78cb648b
SourovKarmokar <sourovkarmokar22@gmail.com>
2025-07-27 03:57:39 +0600

    GroupList & MyGroup

--- src/components/GroupList/GroupList.jsx
+++ src/components/GroupList/GroupList.jsx
@@ -1,15 +1,69 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BsThreeDotsVertical } from 'react-icons/bs';
 import { FaPlus } from 'react-icons/fa';
 import user from "../../assets/user.png"
+import { getDatabase, onValue, push, ref, set } from "firebase/database";
+import { useSelector } from 'react-redux';
 
 const GroupList = () => {
+  const data = useSelector((state) => state.userLogInfo.value.user)
+  const db = getDatabase()
   const [show, setShow] = useState(false);
+  const [groupName, setGroupName] = useState('')
+  const [groupNameErr, setGroupNameErr] = useState('')
 
-  const hendleToggle = () =>{
+  const [groupTagName, setGroupTagName] = useState("")
+  const [groupTagNameErr, setGroupTagNameErr] = useState("")
+
+
+  const handleGroupName = (e) => {
+    setGroupName(e.target.value)
+
+  }
+
+  const hendleToggle = () => {
     console.log('ok');
     setShow(!show)
   }
+
+  const handleCreateGroup = () => {
+    if (!groupName) {
+      setGroupNameErr("Please Give Group Name");
+    }
+    if (!groupTagName) {
+      setGroupTagNameErr("Please Give Group Tag Name")
+    }
+    if (groupName && groupTagName) {
+      set(push(ref(db, 'groups/' )), {
+        groupName : groupName ,
+        groupTagLine : groupTagName,
+        groupAdminId : data.uid,
+      });
+    }
+
+  }
+
+  const handleGroupTagName = (e) => {
+    setGroupTagName(e.target.value)
+  }
+
+
+    const [groupList, setGroupList] = useState([])
+  useEffect(() => {
+    const groupRef = ref(db, 'groups/');
+    onValue(groupRef, (snapshot) => {
+      let arr = []
+      snapshot.forEach((item) => {
+        if(data.uid !== item.val().groupAdminId ){
+          arr.push(item.val())
+        }
+      })
+      setGroupList(arr)
+    });
+
+  }, [])
+
+
 
   return (
     <div className="w-[427px] h-[451px] pt-[20px] pl-[22px] pb-[70px] pr-[25px] rounded-[20px] shadow-[0_4px_4px_rgba(0,0,0,0.25)] font-primary">
@@ -20,11 +74,11 @@ const GroupList = () => {
         {/* <BsThreeDotsVertical /> */}
         {
           show ?
-          <button className='bg-red-300 text-primary rounded py-2 px-3  ' onClick={hendleToggle}>Go Back</button>
-          
-          :
-          <button className='bg-primary text-white rounded py-2 px-3  ' onClick={hendleToggle}>Create Group</button>
-          
+            <button className='bg-red-300 text-primary rounded py-2 px-3  ' onClick={hendleToggle}>Go Back</button>
+
+            :
+            <button className='bg-primary text-white rounded py-2 px-3  ' onClick={hendleToggle}>Create Group</button>
+
         }
 
       </div>
@@ -32,42 +86,48 @@ const GroupList = () => {
       <div className=" overflow-y-auto h-[354px] pt-[10px]">
         {
           show ?
-          <div>
-            <input className='border w-[300px] p-3 rounded-lg' type="text" placeholder='group name' />
-            <input className='border w-[300px] p-3 rounded-lg my-5 ' type="text" placeholder='group tagname' />
-            <br/>
-            <button className="bg-primary text-white rounded py-2 px-3">Submit</button>
-          </div>
-          :
-           <div className="mb-[20px]">
-          <div className="flex h-[54px] justify-between border-b pb-[10px] border-black/25">
-            <div className="flex items-center">
-              <div
-                className="relative w-[52px] h-[52px] rounded-full bg-cover bg-center mr-[10px]"
+            <div>
+              <input value={groupName} onChange={handleGroupName} className='border w-[300px] p-3 rounded-lg' type="text" placeholder='group name' />
+              <p className='text-red-500'>{groupNameErr}</p>
+              <input value={groupTagName} onChange={handleGroupTagName} className='border w-[300px] p-3 rounded-lg my-5 ' type="text" placeholder='group tag name' />
+              <p className='text-red-500'>{groupTagNameErr}</p>
+              <br />
+              <button onClick={handleCreateGroup} className="bg-primary text-white rounded py-2 px-3">Submit</button>
+            </div>
+            :
+            <div>
+              {
+                groupList.map((item) => (
+                  <div className="mb-[20px]">
+              <div className="flex h-[54px] justify-between border-b pb-[10px] border-black/25">
+                <div className="flex items-center">
+                  <div
+                    className="relative w-[52px] h-[52px] rounded-full bg-cover bg-center mr-[10px]"
 
-              >
-                <img src={user} alt="" />
-              </div>
-              <div className='ml-[10px]'>
-                <h2 className="font-primary font-semibold text-black text-[14px]">
-                  Swath Raham
-                </h2>
-                <p className="font-primary font-medium text-[#4D4D4D] opacity-75 text-[12px]">
-                  Today, 8:56pm
-                </p>
+                  >
+                    <img src={user} alt="" />
+                  </div>
+                  <div className='ml-[10px]'>
+                    <h2 className="font-primary font-semibold text-black text-[14px]">
+                      {item.groupName}
+                    </h2>
+                    <p className="font-primary font-medium text-[#4D4D4D] opacity-75 text-[12px]">
+                      {item.groupTagLine}
+                    </p>
+                  </div>
+                </div>
+
+                
               </div>
             </div>
+                ) )
+              }
 
-            <div className="mr-[10px]">
-              <div className="flex size-[30px] bg-black rounded-[5px] justify-center items-center">
-                <FaPlus className="text-white" />
-              </div>
             </div>
-          </div>
-        </div> 
+            
         }
 
-       
+
       </div>
     </div>
 
--- src/components/MyGroup/MyGroup.jsx
+++ src/components/MyGroup/MyGroup.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BsThreeDotsVertical } from 'react-icons/bs';
 import { FaPlus } from 'react-icons/fa';
 import user from "../../assets/user.png"
+import { getDatabase, onValue, ref } from 'firebase/database';
+import { useSelector } from 'react-redux';
 
 const MyGroup = () => {
+   const data = useSelector((state) => state.userLogInfo.value.user)
+  const db = getDatabase()
+  const [mygroupList, setMyGroupList] = useState([])
+  useEffect(() => {
+    const groupRef = ref(db, 'groups/');
+    onValue(groupRef, (snapshot) => {
+      let arr = []
+      snapshot.forEach((item) => {
+        if(data.uid == item.val().groupAdminId ){
+          arr.push(item.val())
+        }
+      })
+      setMyGroupList(arr)
+    });
+
+  }, [])
+ 
+
   return (
     <div className="w-[344px] h-[451px] pt-[20px] pl-[22px] pb-[70px] pr-[25px] rounded-[20px] shadow-[0_4px_4px_rgba(0,0,0,0.25)] font-primary">
       <div className="flex items-center justify-between mb-[34px]">
@@ -14,34 +34,34 @@ const MyGroup = () => {
       </div>
 
       <div className=" overflow-y-auto h-[354px] pt-[10px]">
+        {
+          mygroupList.map((item) => (
+            <div className="mb-[20px]">
+              <div className="flex h-[54px] justify-between border-b pb-[10px] border-black/25">
+                <div className="flex items-center">
+                  <div
+                    className="relative w-[52px] h-[52px] rounded-full bg-cover bg-center mr-[10px]"
 
+                  >
+                    <img src={user} alt="" />
+                  </div>
+                  <div className='ml-[10px]'>
+                    <h2 className="font-primary font-semibold text-black text-[14px]">
+                      {item.groupName}
+                    </h2>
+                    <p className="font-primary font-medium text-[#4D4D4D] opacity-75 text-[12px]">
+                      {item.groupTagLine}
+                    </p>
+                  </div>
+                </div>
 
-        <div className="mb-[20px]">
-          <div className="flex h-[54px] justify-between border-b pb-[10px] border-black/25">
-            <div className="flex items-center">
-              <div
-                className="relative w-[52px] h-[52px] rounded-full bg-cover bg-center mr-[10px]"
-
-              >
-                <img src={user} alt="" />
-              </div>
-              <div className='ml-[10px]'>
-                <h2 className="font-primary font-semibold text-black text-[14px]">
-                  Swath Raham
-                </h2>
-                <p className="font-primary font-medium text-[#4D4D4D] opacity-75 text-[12px]">
-                  Today, 8:56pm
-                </p>
-              </div>
-            </div>
-
-            <div className="mr-[10px]">
-              <div className="flex size-[30px] bg-black rounded-[5px] justify-center items-center">
-                <FaPlus className="text-white" />
+               
               </div>
             </div>
-          </div>
-        </div>
+          ))
+        }
+
+
       </div>
     </div>
 
